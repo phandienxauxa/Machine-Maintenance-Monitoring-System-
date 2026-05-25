daa1b6b3187ae924eb56be76d5b7a4eacf599719
Thái Đức Thiên <thaiducthienthcs@gmail.com>
2026-05-25 18:04:55 +0700

    Bo sung DevExpress C# Dashboard cho he thong

---
+++ Dashboard/demo/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraCharts.SwiftPlotDiagram swiftPlotDiagram1 = new DevExpress.XtraCharts.SwiftPlotDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SwiftPlotSeriesView swiftPlotSeriesView1 = new DevExpress.XtraCharts.SwiftPlotSeriesView();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SwiftPlotSeriesView swiftPlotSeriesView2 = new DevExpress.XtraCharts.SwiftPlotSeriesView();
+            DevExpress.XtraCharts.Series series3 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SwiftPlotSeriesView swiftPlotSeriesView3 = new DevExpress.XtraCharts.SwiftPlotSeriesView();
+            DevExpress.XtraCharts.SwiftPlotDiagram swiftPlotDiagram2 = new DevExpress.XtraCharts.SwiftPlotDiagram();
+            DevExpress.XtraCharts.Series series4 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SwiftPlotSeriesView swiftPlotSeriesView4 = new DevExpress.XtraCharts.SwiftPlotSeriesView();
+            DevExpress.XtraCharts.Series series5 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SwiftPlotSeriesView swiftPlotSeriesView5 = new DevExpress.XtraCharts.SwiftPlotSeriesView();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series6 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.Series series7 = new DevExpress.XtraCharts.Series();
+            chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            arcScaleComponent2 = new DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent();
+            labelComponent2 = new DevExpress.XtraGauges.Win.Base.LabelComponent();
+            circularGauge1 = new DevExpress.XtraGauges.Win.Gauges.Circular.CircularGauge();
+            arcScaleRangeBarComponent2 = new DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleRangeBarComponent();
+            chartControl2 = new DevExpress.XtraCharts.ChartControl();
+            progBattery = new DevExpress.XtraEditors.ProgressBarControl();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            chartControl3 = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)chartControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotDiagram1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)arcScaleComponent2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)labelComponent2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)circularGauge1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)arcScaleRangeBarComponent2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chartControl2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotDiagram2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)progBattery.Properties).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)chartControl3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)xyDiagram1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)series7).BeginInit();
+            SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            swiftPlotDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            swiftPlotDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            swiftPlotDiagram1.EnableAxisXScrolling = true;
+            swiftPlotDiagram1.EnableAxisXZooming = true;
+            swiftPlotDiagram1.EnableAxisYScrolling = true;
+            swiftPlotDiagram1.EnableAxisYZooming = true;
+            chartControl1.Diagram = swiftPlotDiagram1;
+            chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            chartControl1.Location = new System.Drawing.Point(3, 215);
+            chartControl1.Name = "chartControl1";
+            series1.Name = "truc_x";
+            series1.SeriesID = 1;
+            series1.View = swiftPlotSeriesView1;
+            series2.Name = "truc_y";
+            series2.SeriesID = 2;
+            series2.View = swiftPlotSeriesView2;
+            series3.Name = "truc_z";
+            series3.SeriesID = 3;
+            series3.View = swiftPlotSeriesView3;
+            chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[]
+    {
+    series1,
+    series2,
+    series3
+    };
+            chartControl1.Size = new System.Drawing.Size(1061, 620);
+            chartControl1.TabIndex = 0;
+            // 
+            // arcScaleComponent2
+            // 
+            arcScaleComponent2.AppearanceMajorTickmark.BorderBrush = new DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:White");
+            arcScaleComponent2.AppearanceMajorTickmark.ContentBrush = new DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:White");
+            arcScaleComponent2.AppearanceMinorTickmark.BorderBrush = new DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:White");
+            arcScaleComponent2.AppearanceMinorTickmark.ContentBrush = new DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:White");
+            arcScaleComponent2.AppearanceTickmarkText.DXFont = new DevExpress.Drawing.DXFont("Tahoma", 8.5F);
+            arcScaleComponent2.AppearanceTickmarkText.TextBrush = new DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:#484E5A");
+            arcScaleComponent2.Center = new DevExpress.XtraGauges.Core.Base.PointF2D(0F, 0F);
+            arcScaleComponent2.EndAngle = 90F;
+            arcScaleComponent2.MajorTickCount = 0;
+            arcScaleComponent2.MajorTickmark.FormatString = "{0:F0}";
+            arcScaleComponent2.MajorTickmark.ShapeOffset = -14F;
+            arcScaleComponent2.MajorTickmark.ShapeType = DevExpress.XtraGauges.Core.Model.TickmarkShapeType.Circular_Style16_1;
+            arcScaleComponent2.MajorTickmark.TextOrientation = DevExpress.XtraGauges.Core.Model.LabelOrientation.LeftToRight;
+            arcScaleComponent2.MaxValue = 100F;
+            arcScaleComponent2.MinorTickCount = 0;
+            arcScaleComponent2.MinorTickmark.ShapeOffset = -7F;
+            arcScaleComponent2.MinorTickmark.ShapeType = DevExpress.XtraGauges.Core.Model.TickmarkShapeType.Circular_Style16_2;
+            arcScaleComponent2.Name = "scale1";
+            arcScaleComponent2.StartAngle = -270F;
+            arcScaleComponent2.Value = 20F;
+            // 
+            // labelComponent2
+            // 
+            labelComponent2.AppearanceText.DXFont = new DevExpress.Drawing.DXFont("Segoe UI", 27.75F);
+            labelComponent2.Name = "circularGauge1_Label1";
+            labelComponent2.Size = new System.Drawing.SizeF(140F, 60F);
+            labelComponent2.Text = "910";
+            labelComponent2.ZOrder = -1001;
+            // 
+            // circularGauge1
+            // 
+            circularGauge1.Bounds = new System.Drawing.Rectangle(6, 6, 511, 229);
+            circularGauge1.Labels.AddRange(new DevExpress.XtraGauges.Win.Base.LabelComponent[] { labelComponent2 });
+            circularGauge1.Name = "circularGauge1";
+            circularGauge1.Scales.AddRange(new DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent[] { arcScaleComponent2 });
+            // 
+            // arcScaleRangeBarComponent2
+            // 
+            arcScaleRangeBarComponent2.ArcScale = arcScaleComponent2;
+            arcScaleRangeBarComponent2.Name = "circularGauge1_RangeBar2";
+            arcScaleRangeBarComponent2.RoundedCaps = true;
+            arcScaleRangeBarComponent2.ShowBackground = true;
+            arcScaleRangeBarComponent2.StartOffset = 80F;
+            arcScaleRangeBarComponent2.ZOrder = -10;
+            // 
+            // chartControl2
+            // 
+            swiftPlotDiagram2.AxisX.VisibleInPanesSerializable = "-1";
+            swiftPlotDiagram2.AxisY.VisibleInPanesSerializable = "-1";
+            swiftPlotDiagram2.EnableAxisXScrolling = true;
+            swiftPlotDiagram2.EnableAxisXZooming = true;
+            swiftPlotDiagram2.EnableAxisYScrolling = true;
+            swiftPlotDiagram2.EnableAxisYZooming = true;
+            chartControl2.Diagram = swiftPlotDiagram2;
+            chartControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            chartControl2.Location = new System.Drawing.Point(3, 3);
+            chartControl2.Name = "chartControl2";
+            series4.Name = "do_am";
+            series4.SeriesID = 0;
+            series4.View = swiftPlotSeriesView4;
+            series5.Name = "nhiet_do";
+            series5.SeriesID = 1;
+            series5.View = swiftPlotSeriesView5;
+            chartControl2.SeriesSerializable = new DevExpress.XtraCharts.Series[]
+    {
+    series4,
+    series5
+    };
+            chartControl2.Size = new System.Drawing.Size(1061, 206);
+            chartControl2.TabIndex = 1;
+            // 
+            // progBattery
+            // 
+            progBattery.Dock = System.Windows.Forms.DockStyle.Fill;
+            progBattery.Location = new System.Drawing.Point(1070, 3);
+            progBattery.Name = "progBattery";
+            progBattery.Properties.Appearance.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            progBattery.Properties.ShowTitle = true;
+            progBattery.Size = new System.Drawing.Size(371, 206);
+            progBattery.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.89197F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.1080341F));
+            tableLayoutPanel1.Controls.Add(progBattery, 1, 0);
+            tableLayoutPanel1.Controls.Add(chartControl1, 0, 1);
+            tableLayoutPanel1.Controls.Add(chartControl2, 0, 0);
+            tableLayoutPanel1.Controls.Add(chartControl3, 1, 1);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.29833F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.70167F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(1444, 838);
+            tableLayoutPanel1.TabIndex = 3;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // chartControl3
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            chartControl3.Diagram = xyDiagram1;
+            chartControl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            chartControl3.Location = new System.Drawing.Point(1070, 215);
+            chartControl3.Name = "chartControl3";
+            series6.Name = "Thức định kỳ";
+            series6.SeriesID = 0;
+            series7.Name = "Thức do rung";
+            series7.SeriesID = 1;
+            chartControl3.SeriesSerializable = new DevExpress.XtraCharts.Series[]
+    {
+    series6,
+    series7
+    };
+            chartControl3.Size = new System.Drawing.Size(371, 620);
+            chartControl3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1444, 838);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)swiftPlotDiagram1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)arcScaleComponent2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)labelComponent2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)circularGauge1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)arcScaleRangeBarComponent2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotDiagram2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)swiftPlotSeriesView5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartControl2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)progBattery.Properties).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)xyDiagram1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)series7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chartControl3).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleComponent arcScaleComponent2;
+        private DevExpress.XtraGauges.Win.Base.LabelComponent labelComponent2;
+        private DevExpress.XtraGauges.Win.Gauges.Circular.CircularGauge circularGauge1;
+        private DevExpress.XtraGauges.Win.Gauges.Circular.ArcScaleRangeBarComponent arcScaleRangeBarComponent2;
+        private DevExpress.XtraCharts.ChartControl chartControl2;
+        private DevExpress.XtraEditors.ProgressBarControl progBattery;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private DevExpress.XtraCharts.ChartControl chartControl3;
+    }
+}
+
---
+++ Dashboard/demo/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace demo
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        private TcpListener tcpServer;
+        private Thread listenThread;
+        private bool isRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // khoi chay server lang nghe ket noi tu esp32 qua wifi
+            isRunning = true;
+            listenThread = new Thread(new ThreadStart(langNgheKetNoiWifi));
+            listenThread.IsBackground = true;
+            listenThread.Start();
+        }
+
+        // ham nay de fix loi cs0103 bi thieu file form1 load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void langNgheKetNoiWifi()
+        {
+            // bat cong 8888 mo rong cho moi ip trong mang noi bo ket noi den
+            tcpServer = new TcpListener(IPAddress.Any, 8888);
+            tcpServer.Start();
+
+            while (isRunning)
+            {
+                try
+                {
+                    if (tcpServer.Pending())
+                    {
+                        TcpClient client = tcpServer.AcceptTcpClient();
+                        StreamReader reader = new StreamReader(client.GetStream());
+                        string data = reader.ReadLine();
+
+                        if (!string.IsNullOrEmpty(data))
+                        {
+                            this.BeginInvoke(new Action(() =>
+                            {
+                                hienThiLenDashboard(data);
+                            }));
+                        }
+                        client.Close();
+                    }
+                    Thread.Sleep(10); 
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        // ham doc va ve bieu do dev express
+        private void hienThiLenDashboard(string chuoiJson)
+        {
+            try
+            {
+                JObject data = JObject.Parse(chuoiJson);
+
+                // kiem tra loai goi tin type de xu ly phu hop
+                string loaiGoi = (string)data["type"];
+
+                if (data["vbat"] != null && data["pbat"] != null)
+                {
+                    float vbat = (float)data["vbat"];
+                    float pbat = (float)data["pbat"];
+
+                   if (progBattery != null)
+                    {
+                        progBattery.Position = Convert.ToInt32(pbat);
+
+                        if (pbat <= 20)
+                        {
+                            progBattery.ForeColor = System.Drawing.Color.Red; // Dưới 20% -> Đỏ cảnh báo
+                        }
+                        else if (pbat <= 50)
+                        {
+                            progBattery.ForeColor = System.Drawing.Color.Orange; // Dưới 50% -> Vàng cam
+                        }
+                        else
+                        {
+                            progBattery.ForeColor = System.Drawing.Color.Green; // Trên 50% -> Xanh an toàn
+                        }
+                    }
+                }
+
+                if (data["totalWakes"] != null && data["motionWakes"] != null)
+                {
+                    int totalWakes = (int)data["totalWakes"];
+                    int motionWakes = (int)data["motionWakes"];
+                    int timerWakes = totalWakes - motionWakes;
+
+                    if (chartControl3 != null)
+                    {
+                        chartControl3.Series["Thức định kỳ"].Points.Clear();
+                        chartControl3.Series["Thức do rung"].Points.Clear();
+
+                        chartControl3.Series["Thức định kỳ"].Points.AddPoint("Định kỳ", timerWakes);
+                        chartControl3.Series["Thức do rung"].Points.AddPoint("Do Rung", motionWakes);
+                    }
+                }
+
+                if (loaiGoi == "vib")
+                {
+                    // goi tin rung
+                    float ax = (float)data["ax"];
+                    float ay = (float)data["ay"];
+                    float az = (float)data["az"];
+
+                    // cap nhat bieu do mpu6050
+                    chartControl1.Series["truc_x"].Points.AddPoint(DateTime.Now, ax);
+                    chartControl1.Series["truc_y"].Points.AddPoint(DateTime.Now, ay);
+                    chartControl1.Series["truc_z"].Points.AddPoint(DateTime.Now, az);
+
+                    // nhuong bo bot diem cu de bieu do khong bi tran
+                    if (chartControl1.Series["truc_x"].Points.Count > 100)
+                    {
+                        chartControl1.Series["truc_x"].Points.RemoveAt(0);
+                        chartControl1.Series["truc_y"].Points.RemoveAt(0);
+                        chartControl1.Series["truc_z"].Points.RemoveAt(0);
+                    }
+                }
+                else if (loaiGoi == "status")
+                {
+                    float nhietDo = (float)data["temp"];
+                    float doAm = (float)data["hum"];
+
+                    int totalWakes = data["totalWakes"] != null ? (int)data["totalWakes"] : 0;
+                    int motionWakes = data["motionWakes"] != null ? (int)data["motionWakes"] : 0;
+                    int timerWakes = totalWakes - motionWakes;
+
+                    chartControl2.Series["nhiet_do"].Points.AddPoint(DateTime.Now, nhietDo);
+                    chartControl2.Series["do_am"].Points.AddPoint(DateTime.Now, doAm);
+
+                    if (chartControl2.Series["nhiet_do"].Points.Count > 100)
+                    {
+                        chartControl2.Series["nhiet_do"].Points.RemoveAt(0);
+                        chartControl2.Series["do_am"].Points.RemoveAt(0);
+                    }
+
+                    if (chartControl3 != null)
+                    {
+                        chartControl3.Series["Thức định kỳ"].Points.Clear();
+                        chartControl3.Series["Thức do rung"].Points.Clear();
+
+                        chartControl3.Series["Thức định kỳ"].Points.AddPoint("Định kỳ", timerWakes);
+                        chartControl3.Series["Thức do rung"].Points.AddPoint("Do Rung", motionWakes);
+                    }
+                }
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
